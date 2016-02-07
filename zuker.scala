5abd7ea37a80de1829f38715a0bc8216f6381ff1
Alexandr Kravchenko <sasha121196@mail.ru>
2016-02-07 04:04:19 +0400

    Update zuker.scala

--- zuker.scala
+++ zuker.scala
@@ -1,16 +1,7 @@
 class Main {
   val size = 10
-  val matrix_w = for {
-    x <- (1 to size).toList
-  } yield for {
-    y <- (1 to size).toList
-  } yield (x, y)
-
-  val matrix_v = for {
-    x <- (1 to size).toList
-  } yield for {
-    y <- (1 to size).toList
-  } yield (x, y)
+  val matrix_w = Array[Array[Integer]](Array[Integer](3, 4, 5), Array[Integer](1, 2, 6), Array[Integer](7, 8, 9))
+  val matrix_v = Array[Array[Integer]](Array[Integer](3, 2, 1), Array[Integer](4, 5, 1), Array[Integer](2, 3, 2))
   val length = 0
   val sum = 0
   val numbers: scala.collection.immutable.Map[java.lang.String, Array[java.lang.String]] = Map("Phe" -> Array[java.lang.String]("UUA", "UUC", "UUG", "UUU"))
